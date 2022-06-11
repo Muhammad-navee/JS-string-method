78b348f5ec23c49770e3632108c0de2fa238b12b
muhammad naveed <amirprinceak5@gmail.com>
2022-06-11 20:53:05 +0500

    complete

---
+++ app.js
@@ -0,0 +1,118 @@
+//STRING METHODS
+//Q=-1
+
+// var str = prompt ("enter your first name");
+// var str2 = prompt ("enter your last name");
+// var a = str.concat(" " + str2);
+// document.write(a);
+
+//Q-2
+
+// var str = "samsung galaxy S6 edge plus "
+//       var a = str.length;
+      
+//       document.write(" length =" + a);
+
+//Q-3
+
+// var str = "Pakistan";
+//        var a = str.indexOf("n");
+      
+//        document.write("<b> index of 'n' </b>: " + a);
+
+//Q-4
+
+// var str = "Hello World";
+//         var a = str.lastIndexOf("l");
+      
+//         document.write("<b> last index of 'l' :</b>: " + a);
+
+//Q-5
+// var str = prompt("enter a text");
+// var a = str.slice (-1);
+// document.write("last index of : " + a);
+
+//Q=-6
+
+// var str = prompt ("enter your name");
+// var str2 = +prompt ("enter a numer");
+// var a = str.concat(" " + str2);
+// document.write(a);
+
+
+//Q-7
+
+// var str = "Hyderabad ";
+// var a = str.replace("Hyder" , "Islam");
+// document.write("<b> After replacement  :</b>: " + a);
+
+//Q-8
+
+//  var str = "Ali and Sami are best friends. they play cricket and football togethar. ";
+//  var a = str.replace(/and/g, "&");
+//  document.write("<b> After replacement  :</b>: " + a);
+
+//Q-9
+
+//  var str = 472; 
+//  document.write( str + 1);
+
+
+//Q-10
+
+// var str = prompt("Enter a text " );
+// var a = str.toUpperCase();
+// document.write(a);
+
+//Q-11
+
+// var String = prompt("Enter a text " );
+//  var a = String.charAt(0).toUpperCase() + String.slice(1);
+//  document.write(a);
+
+//Q-12
+
+// var numberAsNumber = 35.36; 
+// var numberAsString = numberAsNumber.toString();
+
+
+//   document.write(numberAsString); 
+    
+    //Q-16
+    //  var str = "U N I V E R S T Y OF K A R A C H I ";
+    // var arr = str.split("    ");
+    // console.log(arr);
+
+//Q-14 
+
+
+//  var string = prompt("wellcome to  ABC bakery what do you want to order sir/mam ?");
+//  string = string.toLowerCase();
+// var fruits = [ "cake", "apple pie", "cookie", "chips", "patties"];
+// for (var i = 0; i <= 4; i++) {
+//   if (string === fruits [i]) {
+//     alert("its available ");
+//     break
+
+//   } else {
+//     alert("we are sorry is not available in our bakery ");
+//   break
+//   }
+// }
+
+//console.log(var);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
